c763137d6e68e1daf0a23155413bc0e430ac2e9b
jgkoch <josephgeorgekocheemoolayil@gmail.com>
2024-03-06 01:07:17 +0000

    In line MLUPS

--- src/setup.cpp
+++ src/setup.cpp
@@ -73,8 +73,8 @@ void main_setup() { // 2D Taylor-Green vortices (use D2Q9); required extensions 
 		lbm.u.y[n] = -A*sinf(2.0f*pif*fx/a)*cosf(2.0f*pif*fy/b);
 		lbm.rho[n] = 1.0f;//-sq(A)*3.0f/4.0f*(cosf(4.0f*pif*fx/a)+cosf(4.0f*pif*fy/b));
 	}); // ####################################################################### run simulation, export images and data ##########################################################################
-	lbm.run(1000u);
-        lbm.rho.write_device_to_vtk();
+	lbm.run(25000u);
+        //lbm.rho.write_device_to_vtk();
 } /**/
 
 #endif // BENCHMARK
